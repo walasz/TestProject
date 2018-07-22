940bb476499231526752fa624bec511f5b416aa8
walasz <wanguars@gmail.com>
2018-07-22 12:03:51 +0200

    a felkesz munka

---
+++ src/Data/Teacher.java
@@ -0,0 +1,30 @@
+package Data;
+
+public class Teacher {
+
+		private boolean HeadTeacher;
+		private boolean strict;
+		
+		
+
+		public boolean isHeadTeacher() {
+			return HeadTeacher;
+		}
+		public void setHeadTeacher(boolean headTeacher) {
+			HeadTeacher = headTeacher;
+		}
+		
+		
+		public boolean isStrict() {
+			return strict;
+		}
+		public void setStrict(boolean strict) {
+			this.strict = strict;
+		}
+			
+		
+		
+		
+		
+	
+}
---
+++ src/School_And_Class/School.java
@@ -0,0 +1,10 @@
+package School_And_Class;
+
+import App.Main_Objects;
+
+public class School extends Main_Objects {
+
+	
+	
+	
+}
